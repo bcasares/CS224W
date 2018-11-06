ac6f42f11a78033ee1c07d4988c18c8a2669c1b4
Eric Nielsen <enielsen721@gmail.com>
2018-11-06 11:18:54 -0800

    Cleaned up code

--- geo_data_setup.py
+++ geo_data_setup.py
@@ -16,15 +16,22 @@ PROCESSED_GEO_PATH = 'Data/Geo/sf_geoboundaries.shp'
 ZONE_INFO_CSV_PATH = 'Data/Geo/sf_zone_info.csv'
 MAP_IMAGE_PATH = 'Data/Geo/sf_geoboundaries.png'
 MODIFIED_MAP_IMAGE_PATH = 'Data/Geo/sf_geoboundaries_new.png'
-TRAVEL_TIMES_PATH = 'Data/Travel_Times/sf_hourly_traveltimes_2018_7.csv'
+TRAVEL_TIMES_PATH = 'Data/Travel_Times/sf_hourly_traveltimes_2018_1.csv'
 BORDER_GRAPH_PATH = 'Data/Geo/sf_geoboundaries_borders.graph'
 DISTANCE_GRAPH_PATH = 'Data/Geo/sf_geoboundaries_distances.graph'
+INTERMEDIATE_UBER_GRAPH_PATH = 'Data/Geo/sf_uber_intermediate_graph.graph'
+FINAL_UBER_GRAPH_PATH = 'Data/Geo/sf_uber_final_graph.graph'
 MODIFIED_GRAPH_PATH = 'Data/Geo/TimesDistancesGraph.graph'
 SF_CENTROID = (-122.445515, 37.751943)
-GRAPH_IMAGE_PATH = 'Data/Geo/graph.png'
+UBER_ZONE_BORDER_IMAGE_PATH = 'Data/Geo/sf_uber_zone_borders_image.png'
+FINAL_UBER_GRAPH_IMAGE_PATH = 'Data/Geo/sf_uber_final_image.png'
 
-# Calculate miles between two points
-def haversine(lat1, lon1, lat2, lon2):
+###########################################################################
+###########################################################################
+# Math function: calculate miles between two points
+###########################################################################
+###########################################################################
+def calc_mile_distance(lat1, lon1, lat2, lon2):
     # Convert decimal degrees to radians 
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
     # Apply formula 
@@ -32,7 +39,7 @@ def haversine(lat1, lon1, lat2, lon2):
     dlat = lat2 - lat1 
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a)) 
-    r = 3956 # Radius of earth in kilometers. Use 3956 for miles
+    r = 3956 # Radius of earth in miles.
     return c * r
 
 ###########################################################################
@@ -41,11 +48,9 @@ def haversine(lat1, lon1, lat2, lon2):
 ###########################################################################
 ###########################################################################
 def load_raw_data():
-
     # Open geo boundaries file and read in data
     with open(RAW_GEO_PATH) as f:
         data_raw = json.load(f)
-
     # Boundaries are stored as a list, accessed with 'features' key
     data = data_raw['features']
     return data
@@ -56,7 +61,6 @@ def load_raw_data():
 ###########################################################################
 ###########################################################################
 def load_processed_data():
-
     # Open shp file and return data
     data = fiona.open(PROCESSED_GEO_PATH)
     return data
@@ -67,13 +71,11 @@ def load_processed_data():
 ###########################################################################
 ###########################################################################
 def save_shp(data, radius=None):
-
     # Define a polygon feature geometry with one attribute
     schema = {
         'geometry': 'MultiPolygon',
         'properties': {'id': 'int', 'name': 'str'},
     }
-
     # Save
     with fiona.open(PROCESSED_GEO_PATH, 'w', 'ESRI Shapefile', schema) as c:
         for zone in data:
@@ -81,8 +83,7 @@ def save_shp(data, radius=None):
             if radius:
                 zone_centroid = shape(zone['geometry']).centroid
                 #distance = zone_centroid.distance(Point(SF_CENTROID))
-                distance = haversine(zone_centroid.x, zone_centroid.y, SF_CENTROID[0], SF_CENTROID[1])
-                print(distance)
+                distance = calc_mile_distance(zone_centroid.x, zone_centroid.y, SF_CENTROID[0], SF_CENTROID[1])
                 if distance < radius:
                     c.write({
                         'geometry': zone['geometry'],
@@ -95,12 +96,10 @@ def save_shp(data, radius=None):
 ###########################################################################
 ###########################################################################
 def save_zone_info():
-
     # Extract data
     data = [(zone['properties']['id'], zone['properties']['name'].encode('utf-8')) for zone in fiona.open(PROCESSED_GEO_PATH)]
     polys = MultiPolygon([shape(zone['geometry']) for zone in fiona.open(PROCESSED_GEO_PATH)])
     centroids = [x.centroid for x in polys]
-
     # Save to csv
     with open(ZONE_INFO_CSV_PATH,'wb') as f:
         csv_out = csv.writer(f)
@@ -115,13 +114,11 @@ def save_zone_info():
 ###########################################################################
 ###########################################################################
 def draw_map(filename, zone_filter=None):
-
     # Extract polygons
     if zone_filter:
         polys = MultiPolygon([shape(zone['geometry']) for zone in fiona.open(PROCESSED_GEO_PATH) if zone['properties']['id'] in zone_filter])
     else:
         polys = MultiPolygon([shape(zone['geometry']) for zone in fiona.open(PROCESSED_GEO_PATH)])
-
     # Setup plot
     fig = plt.figure(figsize=(15, 20))
     ax = fig.add_subplot(111)
@@ -130,7 +127,6 @@ def draw_map(filename, zone_filter=None):
     ax.set_xlim(min_x - 0.2 * w, max_x + 0.2 * w)
     ax.set_ylim(min_y - 0.2 * h, max_y + 0.2 * h)
     ax.set_aspect(1)
-
     # Plot, save, and show
     patches = []
     for idx, p in enumerate(polys): patches.append(PolygonPatch(p, fc='#AEEDFF', ec='#555555', alpha=1., zorder=1))
@@ -162,7 +158,7 @@ def create_border_graph(data):
         # Just for tracking progress
         cur = zone1['properties']['id']
         if not cur == prev:
-            print('Finished checking %d of %d zones' % (count, num_nodes))
+            print('[Border graph] Finished checking %d of %d zones' % (count, num_nodes))
             prev = cur
             count += 1
         # The important stuff
@@ -203,16 +199,13 @@ def create_distance_graph(data):
         # Just for tracking progress
         cur = zone1['properties']['id']
         if not cur == prev:
-            print('Finished checking %d of %d zones' % (count, num_nodes))
+            print('[Distance graph] Finished checking %d of %d zones' % (count, num_nodes))
             prev = cur
             count += 1
         # The important stuff
         zone1_id, zone2_id = zone1['properties']['id'], zone2['properties']['id']
-        print("Zone IDs: ", zone1_id, zone2_id)
         zone1_centroid, zone2_centroid = shape(zone1['geometry']).centroid, shape(zone2['geometry']).centroid
-        print("Zone Centroids: ", zone1_centroid, zone2_centroid)
-        distance = zone1_centroid.distance(zone2_centroid)
-        print("Distance: ", distance)
+        distance = calc_mile_distance(zone1_centroid.x, zone1_centroid.y, zone2_centroid.x, zone2_centroid.y)
         if not graph.IsEdge(zone1_id, zone2_id):
             graph.AddEdge(zone1_id, zone2_id, edge_id)
             graph.AddFltAttrDatE(edge_id, distance, 'distance')
@@ -221,7 +214,6 @@ def create_distance_graph(data):
             graph.AddEdge(zone2_id, zone1_id, edge_id)
             graph.AddFltAttrDatE(edge_id, distance, 'distance')
             edge_id += 1
-        raw_input()
     num_edges = graph.GetEdges()
 
     # Print some properties of the graph
@@ -238,61 +230,64 @@ def create_distance_graph(data):
 # Remove edges without time attributes
 ###########################################################################
 ###########################################################################
-
 def modify_distance_graph():
 
-    #Load graph
+    # Load graph
     FIn = snap.TFIn(DISTANCE_GRAPH_PATH)
     graph = snap.TNEANet.Load(FIn)
 
-    #Add time and velocity attributes to edges corresponding to each row in Travel Times csv file
+    # Add time and speed attributes to edges corresponding to each row in Travel Times csv file
     with open(TRAVEL_TIMES_PATH) as f:
         travel_times_reader = csv.reader(f)
         travel_times_reader.next()
-        for row in travel_times_reader:
+        for i, row in enumerate(travel_times_reader):
+            if i % 100000 == 0: print('On row %d' % i)
             try:
                 source_id, dest_id, hour_of_day, mean_travel_time = row[:4]
-                print source_id, dest_id, hour_of_day, mean_travel_time
+                #print source_id, dest_id, hour_of_day, mean_travel_time
                 edge_itr = graph.GetEI(int(source_id), int(dest_id))
-
-                #Add travel time attribute
+                # Add travel time attribute
                 graph.AddFltAttrDatE(edge_itr, float(mean_travel_time), 'travel_time_'+str(hour_of_day))
-
-                #Add speed attribute
+                # Add speed attribute
                 distance = graph.GetFltAttrDatE(edge_itr.GetId(), 'distance')
-                speed = 60*distance/float(mean_travel_time) #in miles per hour
+                speed = 60 * distance/float(mean_travel_time) #in miles per hour
                 graph.AddFltAttrDatE(edge_itr, speed, 'travel_speed_'+str(hour_of_day))
             except:
-                print("Failed")
+                #print("Failed")
+                continue
 
-    print(graph.GetEdges())
-
-    FOut = snap.TFOut("intermediate.graph")
+    # Print number of edges, save intermediate state
+    print('[Intermediate] Num nodes: %d' % graph.GetNodes())
+    print('[Intermediate] Num edges: %d' % graph.GetEdges())
+    FOut = snap.TFOut(INTERMEDIATE_UBER_GRAPH_PATH)
     graph.Save(FOut)
 
-    #Remove edges with no time attribute
+    # Remove edges with no time attributes, then nodes with no connecting edges
     for edge in graph.Edges():
-        print("Edge number " + str(edge.GetId()))
         NameV = snap.TStrV()
         graph.AttrNameEI(edge_itr.GetId(), NameV)
         if len(NameV) < 2:
             graph.DelEdge(edge)
+    for node in graph.Nodes():
+        if node.GetDeg() == 0:
+            graph.DelNode(node.GetId())
 
-    print(graph.GetEdges())
-
-    FOut = snap.TFOut("modified.graph")
+    # Print number of edges, save final state
+    print('[Final] Num nodes: %d' % graph.GetNodes())
+    print('[Final] Num edges: %d' % graph.GetEdges())
+    FOut = snap.TFOut(FINAL_UBER_GRAPH_PATH)
     graph.Save(FOut)
 
-            # NameV = snap.TStrV()
-            # print("Edge ID: " + str(edge_itr.GetId()))
-            # graph.AttrNameEI(edge_itr.GetId(), NameV)
-            # for val in NameV:
-            #     print val
+    # NameV = snap.TStrV()
+    # print("Edge ID: " + str(edge_itr.GetId()))
+    # graph.AttrNameEI(edge_itr.GetId(), NameV)
+    # for val in NameV:
+    #     print val
 
-            # ValueV = snap.TStrV()
-            # graph.AttrValueEI(edge_itr.GetId(), ValueV)
-            # for val in ValueV:
-            #     print val
+    # ValueV = snap.TStrV()
+    # graph.AttrValueEI(edge_itr.GetId(), ValueV)
+    # for val in ValueV:
+    #     print val
 
 ################r###########################################################
 ###########################################################################
@@ -341,11 +336,7 @@ def modify():
 # Draw the uber travel data graph
 ###########################################################################
 ###########################################################################
-def draw_graph():
-
-    # Load graph
-    FIn = snap.TFIn("Data/Geo/smaller.graph")
-    graph = snap.TNEANet.Load(FIn)
+def draw_graph(graph, filename):
 
     # Load zone info
     zone_info = pd.read_csv(ZONE_INFO_CSV_PATH)
@@ -368,26 +359,8 @@ def draw_graph():
 
     nx.draw(nx_graph, pos, node_size=1)
     plt.figure(figsize=(25, 15))
-    plt.savefig(GRAPH_IMAGE_PATH, alpha=True, dpi=300)
+    #plt.savefig(filename, alpha=True, dpi=300)
     plt.show()
-
-################r###########################################################
-###########################################################################
-# Calculate properties of the uber travel data graph
-###########################################################################
-###########################################################################
-def calc_graph_properties():
-
-    # Load graph
-    FIn = snap.TFIn("Data/Geo/smaller.graph")
-    graph = snap.TNEANet.Load(FIn)
-
-    # Times of day
-    hours = range(24)
-
-    # Loop through each 
-
-
 
 ###########################################################################
 ###########################################################################
@@ -399,7 +372,7 @@ def main():
     # Step 1: Load raw data and convert boundaries to .shp file
     if False:
         data = load_raw_data()
-        save_shp(data, radius=14)
+        save_shp(data, radius=14) # Radius of 14 miles from center of SF
 
     # Step 2: Save csv file mapping zone ids to zone names and zone centroids
     if False:
@@ -410,7 +383,7 @@ def main():
         draw_map(filename=MAP_IMAGE_PATH)
 
     # Step 4: Create spatial SNAP graph from zones and borders
-    if False:
+    if True:
         data = load_processed_data()
         create_border_graph(data)
 
@@ -419,7 +392,7 @@ def main():
         data = load_processed_data()
         create_distance_graph(data)
 
-    #Step 6: Add time attributes to graph, remove unncesseary edges
+    # Step 6: Add time / speed attributes to graph, remove unncesseary edges and nodes
     if False:
         modify_distance_graph()
 
@@ -430,8 +403,20 @@ def main():
     if False:
         modify()
 
+    # Draw border uber graph (edges based on zone borders)
     if True:
-        draw_graph()
+        # Load graph 
+        FIn = snap.TFIn(BORDER_GRAPH_PATH)
+        graph = snap.TNEANet.Load(FIn)
+        print('Loaded graph')
+        draw_graph(graph, UBER_ZONE_BORDER_IMAGE_PATH)
+
+    # Draw final uber graph (edges based on trips)
+    if True:
+        # Load graph 
+        FIn = snap.TFIn(FINAL_UBER_GRAPH_PATH)
+        graph = snap.TNEANet.Load(FIn)
+        draw_graph(graph, FINAL_UBER_GRAPH_IMAGE_PATH)
 
 if __name__ == "__main__":
     main()
