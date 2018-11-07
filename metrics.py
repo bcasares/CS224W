af95563953006d07fd8057bfb4f488df028cad59
Ammar Alqatari <ammar.qat@gmail.com>
2018-11-06 17:25:29 -0800

    node classification

--- metrics.py
+++ metrics.py
@@ -1,5 +1,11 @@
 from util import *
 
+from collections import defaultdict
+import numpy as np
+from sklearn.metrics import pairwise_distances_argmin
+from scipy.stats import wasserstein_distance
+
+
 # Source files / other
 RAW_GEO_PATH = 'Data/Geo/sf_geoboundaries.json'
 PROCESSED_GEO_PATH = 'Data/Geo/sf_geoboundaries.shp'
@@ -81,6 +87,52 @@ def compute_node_degree(original_graph, attribute, average=False, only_zone_neig
 
 ###########################################################################
 ###########################################################################
+#Get edge weights for node
+###########################################################################
+###########################################################################
+def get_edge_weights(original_graph, attribute):
+
+    # Create new graph using desired attribute
+    graph = build_single_weight_graph(original_graph, attribute)
+
+    edgeWeights = defaultdict(list)
+    # Loop through all nodes, add attribute to each that is the sum of all adjacent edge weights
+    for node in graph.Nodes():
+        node_id, num_out_nodes = node.GetId(), node.GetOutDeg()
+        degree = 0
+        for i in range(num_out_nodes):
+            neighbor_id = node.GetOutNId(i)
+            edge_id = graph.GetEI(node_id, neighbor_id).GetId()
+            weight = graph.GetFltAttrDatE(edge_id, 'weight')
+            if weight > 0: 
+                edgeWeights[node_id].append(weight) # For some reason a few weights are -inf
+
+    # Return
+    return edgeWeights
+
+def find_clusters(X, n_clusters, rseed=2):
+        # 1. Randomly choose clusters
+    rng = np.random.RandomState(rseed)
+    i = rng.permutation(X.shape[0])[:n_clusters]
+    centers = X[i]
+    
+    while True:
+        # 2a. Assign labels based on closest center
+        labels = pairwise_distances_argmin(X, centers, metric=wasserstein_distance)
+        
+        # 2b. Find new centers from means of points
+        new_centers = np.array([X[labels == i].mean(0)
+                                for i in range(n_clusters)])
+        
+        # 2c. Check for convergence
+        if np.all(centers == new_centers):
+            break
+        centers = new_centers
+    
+    return centers, labels
+
+###########################################################################
+###########################################################################
 # Main function
 ###########################################################################
 ###########################################################################
@@ -101,7 +153,7 @@ def main():
                         plot_centroids=True, scale_centroids=True, graph=new_graph)
 
     # Compute average node degree over time
-    if True:
+    if False:
         # Load graph 
         FIn = snap.TFIn(FINAL_UBER_GRAPH_PATH)
         original_graph = snap.TNEANet.Load(FIn)
@@ -127,6 +179,31 @@ def main():
             plt.ylabel('Average ' + ' '.join(attribute.split('_')[:2]) + '(' + label + ')')
             plt.title('Average ' + ' '.join(attribute.split('_')[:2]) + ' by Hour of Day')
             plt.savefig('Data/Geo/Images/' + attribute + '_avg_by_hour')
+
+    if True:
+        FIn = snap.TFIn(FINAL_UBER_GRAPH_PATH)
+        original_graph = snap.TNEANet.Load(FIn)
+        edgeWeightDistributions = []
+        attributes = ['travel_time_12']
+        for attribute in attributes:
+            edgeWeights = get_edge_weights(original_graph, attribute)
+            #1. Convert each node array of edges to histogram
+                #Find global min and max values of weights
+            minWeight = min([min(weights) for node, weights in edgeWeights.iteritems()])
+            maxWeight = max([max(weights) for node, weights in edgeWeights.iteritems()])
+            edgeWeightHistograms = {}
+            for node, weights in edgeWeights.iteritems():
+                histo = np.histogram(weights, bins=[  21.65,341.75083333,661.85166667,981.9525,1302.05333333, 1622.15416667, 1942.255, 2262.35583333,2582.45666667, 2902.5575,3222.65833333,3542.75916667,3862.86],
+                    range=(minWeight, maxWeight))
+                edgeWeightHistograms[node] = list(histo[0])
+
+            nodes, histograms = zip(*edgeWeightHistograms.iteritems())
+            centers, labels = find_clusters(np.array(histograms), 3)
+            node_roles = dict(zip(nodes, labels))
+            draw_map(attribute+'_node_roles', plot_centroids=True, centroid_classes=node_roles)
+
+
+
 
 if __name__ == "__main__":
     main()
